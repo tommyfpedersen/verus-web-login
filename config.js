d7e877634592a1d62f81f528157d383bafc2d256
tommyfpedersen <142407342+tommyfpedersen@users.noreply.github.com>
2026-03-26 15:17:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const VERUS_SERVER_API = "http://192.168.1.121:8000";
-export const VERUS_SERVER_WS = "ws://192.168.1.121:8000";
+export const VERUS_LOGIN_SERVER_API = "http://192.168.1.1:8000";
+export const VERUS_LOGIN_SERVER_WS = "ws://192.168.1.1:8000";
